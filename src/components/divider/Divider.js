4724d6af10b8b7b0c66477327d925893b55eb0c4
SabiMantock <sabimantock@gmail.com>
2023-02-02 11:10:16 +0000

    feat/full-website: complete main features: - Add styles to improve the look and feel - Create a Route for each page - Layout matches wireframes

---
+++ src/components/divider/Divider.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Divider.css';
+
+const Divider = () => (
+  <div className="divider" />
+);
+
+export default Divider;
---
+++ src/pages/Quotes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Quotes.css';
+
+const Quotes = () => (
+  <div className="qcontainer">
+    <h2>
+      Mathematics is not about numbers,
+      equations, computations,
+      <br />
+      or algorithms:
+      it is about understanding.
+      -William Paul Thurston
+
+    </h2>
+  </div>
+);
+
+export default Quotes;
